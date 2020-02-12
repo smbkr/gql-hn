11de419c61401d1c7481e664b909711f99720c3e
Stuart Baker <stuart.baker1@sainsburys.co.uk>
2020-02-12 10:32:26 +0000

    Basic HN clone with feed and create support

---
+++ src/index.js
@@ -0,0 +1,34 @@
+const { GraphQLServer } = require('graphql-yoga');
+
+let links = [
+  {
+    id: 'link-0',
+    url: 'www.howtographql.com',
+    description: 'Fullstack tutorial for GraphQL',
+  },
+];
+
+const resolvers = {
+  Query: {
+    info: () => 'This is the API for a Hacker News clone.',
+    feed: () => links,
+  },
+  Mutation: {
+    post: (parent, args) => {
+      const link = {
+        id: `link-${links.length++}`,
+        description: args.description,
+        url: args.url,
+      };
+      links.push(link);
+
+      return link;
+    },
+  },
+};
+
+const server = new GraphQLServer({
+  typeDefs: './src/schema.graphql',
+  resolvers,
+});
+server.start(() => console.log('Server listening on http://localhost:4000'));
