9c72242b9631ecdfb1573739efce221d70c21fb0
Stuart Baker <stuart.baker1@sainsburys.co.uk>
2020-02-17 09:51:27 +0000

    Add link submission component

---
+++ client/src/components/CreateLink.js
@@ -0,0 +1,48 @@
+import React, { useCallback, useState } from 'react';
+import { useMutation } from 'urql';
+import gql from 'graphql-tag';
+
+const CreateLink = () => {
+  const [description, setDescription] = useState('');
+  const [url, setUrl] = useState('');
+
+  const [state, executeMutation] = useMutation(query);
+
+  const submit = useCallback(() => {
+    executeMutation({ url, description });
+  }, [executeMutation, url, description]);
+
+  return (
+    <div>
+      <div className="flex flex-column mt3">
+        <input
+          type="text"
+          className="mb2"
+          value={description}
+          onChange={e => setDescription(e.target.value)}
+          placeholder="A description for the link"
+        />
+        <input
+          type="text"
+          className="mb2"
+          value={url}
+          onChange={e => setUrl(e.target.value)}
+          placeholder="The URL you wish to link to"
+        />
+      </div>
+      <button onClick={submit} disabled={state.fetching}>
+        Submit Link
+      </button>
+    </div>
+  );
+};
+
+const query = gql`
+  mutation CreateLink($description: String!, $url: String!) {
+    post(description: $description, url: $url) {
+      id
+    }
+  }
+`;
+
+export default CreateLink;
